059301d3682e03b10e12473590c9e8a1ff0f4e5f
Kunal D Patil <kevindpatil@gmail.com>
2021-08-23 15:40:40 +0530

    first commit

---
+++ EMS/myapp/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from .forms import SignUpFrom, LoginForm
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import logout
+from datetime import datetime
+# Create your views here.
+
+def user_signup(request):
+    if request.method == 'POST':
+        fm = SignUpFrom(request.POST, request.FILES)
+        if fm.is_valid():
+            fm.save()
+            return redirect('/')
+    else:
+        fm = SignUpFrom()
+    return render(request, 'myapp/signup.html', {'form':fm})
+
+def age_calculate(date):
+    return (datetime.date.today()-date).days/365
+    # return int((datetime.date.today() - date).days / 365.25)
+
+
+def profile(request):
+    if request.user.is_authenticated:
+        dob = request.user.date_of_birth
+        age = age_calculate(dob)
+        print(age)
+        return render(request, 'myapp/profile.html', {'emp': age})
+    else:
+        return redirect('/login/')
+
+
+
+class LoginView(auth_views.LoginView):
+    form_class = LoginForm
+    template_name = 'myapp/login.html'
+
+
+def profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'myapp/profile.html')
+    else:
+        return redirect('/login/')
+
+def user_logout(request):
+    logout(request)
+    return redirect('/login/')
+
+
+class PasswordChangeView(auth_views.PasswordChangeView):
+    template_name = 'myapp/changepass.html'
+    success_url = '/changepassdone/'
+
+class PasswordChangeDoneView(auth_views.PasswordChangeDoneView):
+    template_name = 'myapp/changepassdone.html'
